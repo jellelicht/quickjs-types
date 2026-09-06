2141bab879e9fd337febe93de69ded635adf035c
Jelle Licht <jlicht@fsfe.org>
2026-09-06 15:11:02 +0200

    Add os.getpid()

--- os.d.ts
+++ os.d.ts
@@ -140,6 +140,7 @@ declare module "os" {
   export function signal(signal: number, cb: undefined): void;
   export function kill(pid: number, signal: number): void;
   export function exec(args: string[], options?: ExecOptions): number;
+  export function getpid(): number;
   export function waitpid(
     pid: number,
     options: number
